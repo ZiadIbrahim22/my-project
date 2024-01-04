b1ea16f1218131ebf2f04092898b43cbf3eadd69
Ziad Ibrahim <155671525+ZiadIbrahim22@users.noreply.github.com>
2024-01-04 20:46:18 +0200

    Add files via upload

---
+++ CLL/CLL/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLL
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data) { this.data = data; }
+        public Node() {  }
+    }
+
+    class CLL
+    {
+        Node last = null;
+
+        public void append(int Data)
+        {
+            Node newnode = new Node();
+            newnode.data = Data;
+            if (last == null)
+            {
+                last = newnode;
+                newnode.next = newnode;
+            }
+            newnode.next = last.next;
+            last.next = newnode;
+            last = newnode;
+        }
+        public void addfront(int Data)
+        {
+            Node newnode = new Node(Data);
+            newnode.next = last.next;
+            last.next = newnode;
+        }
+
+        public void print()
+        {
+            Node newnode = last.next;
+            while (newnode != last)
+            {
+                Console.Write(newnode.data + " ");
+                newnode = newnode.next;
+            }
+            Console.WriteLine(newnode.data);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CLL/CLL/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CLL c = new CLL();
+
+            Console.Write("Add Element At End : ");
+            int n1 = int.Parse(Console.ReadLine());
+            c.append(n1);
+            c.print();
+
+            Console.Write("Add Element At End : ");
+            int n2 = int.Parse(Console.ReadLine());
+            c.append(n2);
+            c.print();
+
+            Console.Write("Add Element At End : ");
+            int n3 = int.Parse(Console.ReadLine());
+            c.append(n3);
+            c.print();
+
+            Console.Write("Add Element At Front : ");
+            int n4 = int.Parse(Console.ReadLine());
+            c.addfront(n4);
+            c.print();
+
+
+            Console.ReadKey();
+        }
+    }
+}
